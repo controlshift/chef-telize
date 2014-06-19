f8ab7ad30ce8be1a77fe2298b51a363758c4988c
Jacinda Moore <alynna.kasmira@gmail.com>
2014-06-19 12:01:34 -0400

    Use ipv4-only geoip databases

--- recipes/update_geoip_dbs.rb
+++ recipes/update_geoip_dbs.rb
@@ -2,17 +2,24 @@
 # Cookbook Name:: telize
 # Recipe:: update_geoip_dbs
 # Installs the latest version of the GeoIP databases.
-#
+
 destination_dir = '/usr/share/GeoIP/'
-sources_by_file = {'GeoIPv6.dat' => 'http://geolite.maxmind.com/download/geoip/database/GeoIPv6.dat.gz',
-                   'GeoLiteCityv6.dat' => 'http://geolite.maxmind.com/download/geoip/database/GeoLiteCityv6-beta/GeoLiteCityv6.dat.gz',
-                   'GeoIPASNumv6.dat' => 'http://download.maxmind.com/download/geoip/database/asnum/GeoIPASNumv6.dat.gz'}
+
+# Ubuntu 12.04's version of libgeoip can't handle ipv6, so we just use the ipv4-only databases
+# TODO: This could be a cookbook attribute
+#sources_by_file = {'GeoIPv6.dat' => 'http://geolite.maxmind.com/download/geoip/database/GeoIPv6.dat.gz',
+#                   'GeoLiteCityv6.dat' => 'http://geolite.maxmind.com/download/geoip/database/GeoLiteCityv6-beta/GeoLiteCityv6.dat.gz',
+#                   'GeoIPASNumv6.dat' => 'http://download.maxmind.com/download/geoip/database/asnum/GeoIPASNumv6.dat.gz'}
+sources_by_file = {'GeoIP.dat' => 'http://geolite.maxmind.com/download/geoip/database/GeoLiteCountry/GeoIP.dat.gz',
+                   'GeoLiteCity.dat' => 'http://geolite.maxmind.com/download/geoip/database/GeoLiteCity.dat.gz',
+                   'GeoIPASNum.dat' => 'http://download.maxmind.com/download/geoip/database/asnum/GeoIPASNum.dat.gz'}
+
 # In testing we sometimes get 403s from rate limiting on maxmind.
 # One workaround is to use a simple server locally to serve the zipped versions of the files
 # Uncomment the below and replace with the right IP address to use such an alternate server.
-#sources_by_file = {'GeoIPv6.dat' => 'http://192.168.0.237:8000/GeoIPv6.dat.gz',
-#                   'GeoLiteCityv6.dat' => 'http://192.168.0.237:8000/GeoLiteCityv6.dat.gz',
-#                   'GeoIPASNumv6.dat' => 'http://192.168.0.237:8000/GeoIPASNumv6.dat.gz'}
+#sources_by_file = {'GeoIP.dat' => 'http://192.168.0.237:8000/GeoIP.dat.gz',
+#                   'GeoLiteCity.dat' => 'http://192.168.0.237:8000/GeoLiteCity.dat.gz',
+#                   'GeoIPASNum.dat' => 'http://192.168.0.237:8000/GeoIPASNum.dat.gz'}
 
 directory destination_dir do
     action :create
@@ -23,8 +30,9 @@ sources_by_file.each do |destination_filename, source_url|
     zipped_filepath = "#{Chef::Config['file_cache_path']}/#{zipped_filename}"
 
     # Retrieve the zipped DB, telling the server to only bother if it has changed since we last asked.
-    # TODO: If the server is down, this will fail the whole recipe.  It would be nice to have a fallback
-    # where if we have /any/ copy of the DB already (zipped, old, whatever), we use that and move on.
+    # TODO: If the server is down or we've been rate-limited, this will fail the whole recipe.  It would
+    # be nice to have a fallback where if we have /any/ copy of the DB already (zipped, old, whatever), we
+    # use that and move on.
     remote_file zipped_filepath do
         source source_url
         use_conditional_get true
