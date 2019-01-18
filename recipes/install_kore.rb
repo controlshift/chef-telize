99db9486033d82dc7f1a7af51a6c0dd73d8a9aa2
Jacinda Moore <jacinda@controlshiftlabs.com>
2019-01-18 12:24:07 -0500

    can't use tar_package because kore has no configure step

--- recipes/install_kore.rb
+++ recipes/install_kore.rb
@@ -1,1 +1,15 @@
-tar_package 'https://kore.io/releases/kore-3.2.1.tar.gz'
+remote_file '/tmp/kore-tarball.tar.gz' do
+  source 'https://kore.io/releases/kore-3.2.1.tar.gz'
+  #verify 'minisign -Vm %{path} -P RWSxkEDc2y+whfKTmvhqs/YaFmEwblmvar7l6RXMjnu6o9tZW3LC0Hc9'
+end
+
+tar_extract '/tmp/kore-tarball.tar.gz' do
+  action :extract_local
+  target_dir '/tmp'
+  creates '/tmp/kore-3.2.1'
+end
+
+bash 'install kore' do
+  code 'make && make install'
+  cwd '/tmp/kore-3.2.1'
+end
