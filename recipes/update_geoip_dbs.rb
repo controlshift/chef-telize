2461b62def916d9475cdf9f1502f6f7edab22153
Jacinda Moore <jacinda@controlshiftlabs.com>
2019-01-18 16:22:31 -0500

    telize expects files to be unpacked from geoip downloads

--- recipes/update_geoip_dbs.rb
+++ recipes/update_geoip_dbs.rb
@@ -5,11 +5,18 @@ directory directory_path do
 end
 
 tar_extract 'https://geolite.maxmind.com/download/geoip/database/GeoLite2-City.tar.gz' do
-  target_dir directory_path
+  target_dir '/tmp'
   compress_char 'z'
 end
 
 tar_extract 'https://geolite.maxmind.com/download/geoip/database/GeoLite2-ASN.tar.gz' do
-  target_dir directory_path
+  target_dir '/tmp'
   compress_char 'z'
+end
+
+bash 'copy files to where telize expects them to be' do
+  code <<~CODE
+  cp /tmp/GeoLite2-City_*/GeoLite2-City.mmdb #{directory_path}/
+  cp /tmp/GeoLite2-ASN_*/GeoLite2-ASN.mmdb #{directory_path}/
+  CODE
 end
