b81408b875867cfd721a248b45f7d4758e400677
Jacinda Moore <jacinda@controlshiftlabs.com>
2019-01-18 12:24:19 -0500

    fix directory creation

--- recipes/update_geoip_dbs.rb
+++ recipes/update_geoip_dbs.rb
@@ -1,6 +1,8 @@
 directory_path = '/var/db/GeoIP'
 
-directory directory_path
+directory directory_path do
+  recursive true
+end
 
 tar_extract 'https://geolite.maxmind.com/download/geoip/database/GeoLite2-City.tar.gz' do
   target_dir directory_path
