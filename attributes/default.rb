0ce1628f68b8c83acf9d1517e527e3a3c773b50c
Jacinda Moore <jacinda@controlshiftlabs.com>
2019-01-21 11:29:35 -0500

    Remove references to attributes we no longer support

--- attributes/default.rb
+++
@@ -1,5 +0,0 @@
-# Assume calls from this subnet are from our LB, so we should use the x-forwarded-for IP address
-default['telize']['lb_subnet'] = '172.31.32.0/18'
-
-# Should we support IPv6?
-default['telize']['ipv6?'] = true
