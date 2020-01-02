7fb9419dad8a211c49b11d9c79793f07de7242cc
Jacinda Moore <jacinda@controlshiftlabs.com>
2020-01-02 17:20:11 -0500

    fix paths for geoip conf & bin

--- recipes/update_geoip_dbs.rb
+++ recipes/update_geoip_dbs.rb
@@ -9,7 +9,7 @@ end
 apt_package 'geoipupdate'
 
 # TODO: Figure out if the EditionIDs are all needed, or can we skip some?
-template '/usr/local/etc/GeoIP.conf' do
+template '/etc/GeoIP.conf' do
   source 'GeoIP.conf.erb'
   variables({ directory_path: directory_path })
 end
@@ -19,7 +19,5 @@ cron 'run geoipupdate' do
   weekday '0'
   hour '4'
   minute '0'
-  #shell '/bin/bash'
-  #path '/usr/local/sbin:/usr/local/bin:/sbin:/bin:/usr/sbin:/usr/bin'
-  command '/usr/local/bin/geoipupdate'
+  command '/usr/bin/geoipupdate'
 end
