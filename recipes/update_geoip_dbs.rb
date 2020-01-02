cc2dfd611474332d31ba96b469390473aa43aeeb
Jacinda Moore <jacinda@controlshiftlabs.com>
2020-01-02 16:02:08 -0500

    WiP new way to download geoip databases

--- recipes/update_geoip_dbs.rb
+++ recipes/update_geoip_dbs.rb
@@ -4,20 +4,47 @@ directory directory_path do
   recursive true
 end
 
-tar_extract 'https://geolite.maxmind.com/download/geoip/database/GeoLite2-City.tar.gz' do
-  target_dir '/tmp'
-  compress_char 'z'
+apt_package 'geoipupdate' do
+  notifies :create, 'template[/usr/local/etc/GeoIP.conf]', :immediately
 end
 
-tar_extract 'https://geolite.maxmind.com/download/geoip/database/GeoLite2-ASN.tar.gz' do
-  target_dir '/tmp'
-  compress_char 'z'
+template '/usr/local/etc/GeoIP.conf' do
+  source 'GeoIP.conf.erb'
+  variables({
+              account_id: account_id,
+              license_key: license_key,
+              edition_ids: edition_ids,
+              directory_path: directory_path
+            })
+
+  action :nothing
+  notifies :run, 'bash[geoipupdate]', :immediately
 end
 
-bash 'copy files to where telize expects them to be' do
+bash 'geoipupdate' do
   code <<~CODE
-  cp /tmp/GeoLite2-City_*/GeoLite2-City.mmdb #{directory_path}/
-  cp /tmp/GeoLite2-ASN_*/GeoLite2-ASN.mmdb #{directory_path}/
-  chown -R telize:telize #{directory_path}
+  /usr/local/bin/geoipupdate
   CODE
+
+  action :nothing
 end
+
+# TODO: set up a cron job to run geoipupdate weekly
+
+#tar_extract 'https://geolite.maxmind.com/download/geoip/database/GeoLite2-City.tar.gz' do
+#  target_dir '/tmp'
+#  compress_char 'z'
+#end
+#
+#tar_extract 'https://geolite.maxmind.com/download/geoip/database/GeoLite2-ASN.tar.gz' do
+#  target_dir '/tmp'
+#  compress_char 'z'
+#end
+#
+#bash 'copy files to where telize expects them to be' do
+#  code <<~CODE
+#  cp /tmp/GeoLite2-City_*/GeoLite2-City.mmdb #{directory_path}/
+#  cp /tmp/GeoLite2-ASN_*/GeoLite2-ASN.mmdb #{directory_path}/
+#  chown -R telize:telize #{directory_path}
+#  CODE
+#end
