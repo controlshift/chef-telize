f2c3580f3e04c6b5b689f6d5da91ee367df5f25a
Jacinda Moore <jacinda@controlshiftlabs.com>
2020-01-03 09:43:06 -0500

    overwrite conf file after package is installed

--- recipes/update_geoip_dbs.rb
+++ recipes/update_geoip_dbs.rb
@@ -6,12 +6,17 @@ directory directory_path do
   recursive true
 end
 
-apt_package 'geoipupdate'
+# This package will install a default /etc/GeoIP.conf, so we must
+# wait until it's installed to overwrite it with our own.
+apt_package 'geoipupdate' do
+  notifies :create, 'template[/etc/GeoIP.conf]', :immediately
+end
 
 # TODO: Figure out if the EditionIDs are all needed, or can we skip some?
 template '/etc/GeoIP.conf' do
   source 'GeoIP.conf.erb'
   variables({ directory_path: directory_path })
+  action :nothing
 end
 
 cron 'run geoipupdate' do
