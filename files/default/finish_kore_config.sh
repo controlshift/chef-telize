123423b42a156bf51df15aed7879a03f5e5680c2
Jacinda Moore <jacinda@controlshiftlabs.com>
2020-01-06 11:23:56 -0500

    update private IP lookup stanza to reflect new format of ifconfig output

--- files/default/finish_kore_config.sh
+++ files/default/finish_kore_config.sh
@@ -4,7 +4,7 @@ set -e
 set -x
 
 NET_IF=$(ip link | awk -F: '$0 !~ "lo|docker|tun|^[^0-9]"{print $2;getline}')
-PRIVATE_IP=$(ifconfig $NET_IF 2>/dev/null | awk '/inet addr:/ {print $2}' | sed 's/addr://')
+PRIVATE_IP=$(ifconfig $NET_IF 2>/dev/null | awk '/inet / {print $2}' | sed 's/addr://')
 
 sed -i -e "s%127.0.0.1%$PRIVATE_IP%" /srv/telize/telize.config
 
