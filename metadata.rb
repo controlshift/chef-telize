2f599948ac2d2d4a5bf78094bfaa7e5370a035ec
Grey Moore <grey@controlshiftlabs.com>
2022-04-26 12:03:29 -0500

    bump version to 2.3.2

--- metadata.rb
+++ metadata.rb
@@ -4,6 +4,6 @@ maintainer_email 'talk@controlshiftlabs.com'
 license          'All rights reserved'
 description      'Installs/Configures telize'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '2.3.1'
+version          '2.3.2'
 
 depends 'tar'
